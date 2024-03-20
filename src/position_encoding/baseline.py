b7518c8f24b4eb7157ff9e6473a402aa9aa68850
nir.yarden <nir.yarden@mail.huji.ac.il>
2024-03-20 17:11:31 +0200

    bugfix

--- src/position_encoding/baseline.py
+++ src/position_encoding/baseline.py
@@ -2,4 +2,4 @@ from TOVA.src.position_encoding.position_encoding_indexes import PositionEncodin
 
 class BaselinePositionEncodingIndexes(PositionEncodingIndexes):
     def get_position_indexes(self, past_key_value, layer_idx, context_limit):
-        return past_key_value.cached_input_indices[layer_idx]
+        return past_key_value.cached_input_indexes[layer_idx]
